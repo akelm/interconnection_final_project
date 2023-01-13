66a036d5b53ae39ec190d741d2a41e40d32c60a5
starcatter <masterfoo@o2.pl>
2023-01-13 20:43:24 +0100

    alternate startup script

--- src/main/python/run_python.sh
+++ src/main/python/run_python.sh
@@ -1,5 +1,8 @@
 [[ -d venv ]] || python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/ python3 -m main
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk/
+export JVM_PATH=/usr/lib/jvm/java-8-openjdk/jre/lib/amd64/server/libjvm.so
+
+python3 -m main
 python3 -m plotting
